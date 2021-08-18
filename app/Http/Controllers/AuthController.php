8c3af594215ddcfae22891f2bacf2b1aeeec9ce4
franciscomarin98 <francmarinc@gmail.com>
2021-08-18 10:50:10 -0500

    Uploading the login and registration routes, they work but need to be optimized

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,9 +4,11 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\UserStoreRequest;
 use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -16,13 +18,48 @@ class AuthController extends Controller
      */
     public function register(UserStoreRequest $request): JsonResponse
     {
-        $user = User::create($request->validated());
+
+        $requestData = $request->all();
+        $requestData['password'] = bcrypt($requestData['password']);
+
+        $user = User::create($requestData);
+        $tokenResult = $user->createToken('Personal Access Token');
+
         return response()->json([
             'status' => true,
             'code' => Response::HTTP_CREATED,
             'message' => 'Welcome new user, successful registration',
             'token_type' => 'Bearer',
-            '_token' => $user->createToken('Personal Access Token')->accessToken,
+            'access_token' => $tokenResult->accessToken,
+            'expires_at' => Carbon::parse($tokenResult->token->expires_at)->toDateTimeString()
         ], Response::HTTP_CREATED);
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+            'remember_me' => 'boolean'
+        ]);
+
+        $credentials = request(['email', 'password']);
+
+        if (!Auth::attempt($credentials)){
+            return response()->json([
+                'status' => false,
+                'code' => Response::HTTP_UNAUTHORIZED,
+                'message' => 'Unauthorized'
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+
+        $user = $request->user();
+        $tokenResult = $user->createToken('Personal Access Token');
+
+        return response()->json([
+            'token_type' => 'Bearer',
+            'access_token' => $tokenResult->accessToken,
+            'expires_at' => Carbon::parse($tokenResult->token->expires_at)->toDateTimeString()
+        ]);
     }
 }
